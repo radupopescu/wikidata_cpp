71b4358599efbe345dc4935fd1fbd63a50f782de
Radu Popescu <mail@radupopescu.net>
2016-02-20 18:54:08 +0100

    Refactored parseItem function to use RapidJSON

--- src/data.cpp
+++ src/data.cpp
@@ -9,27 +9,37 @@
 
 #include "data.h"
 
-#include <json.hpp>
+#include <rapidjson/Document.h>
 
-using json = nlohmann::json;
+#include <iostream>
+
+namespace rj = rapidjson;
 
 namespace data {
 
 bool parseItem(const Languages& languages, const std::string& line, WikidataElement& elem)
 {
-  try {
-    const auto j = json::parse(line.substr(0, line.size() - 1));
-    WikidataElement el;
-    el.id = j.at("id");
-    const auto& s = j.at("sitelinks");
-    for (const auto& l : languages) {
-      el.sites[l] = s.at(l + "wiki").at("title");
+  rj::Document j;
+  j.Parse(line.substr(0, line.size() - 1).c_str());
+  if (! j.HasParseError()) {
+    if (j.HasMember("id")) {
+      elem.id = j["id"].GetString();
+      if (j.HasMember("sitelinks")) {
+        const auto& s = j["sitelinks"].GetObject();
+        for (const auto& l : languages) {
+          const auto sitelink = l + "wiki";
+          if (s.HasMember(sitelink.c_str())) {
+            if (s[sitelink.c_str()].HasMember("title")) {
+              elem.sites[l] = s[sitelink.c_str()]["title"].GetString();
+            }
+          }
+        }
+        return elem.sites.size() == languages.size();
+      }
     }
-    elem = el;
-    return true;
-  } catch (std::exception& e) {
-    return false;
   }
+
+  return false;
 }
 
 }
